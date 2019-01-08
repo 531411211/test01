75a124b6044ede969d00dea858fbe38380b7c9c0
“wei wang <wangwei@egridcloud.com>
2019-01-08 23:52:53 +0800

    添加一个文件

---
+++ src/main/java/com/wangwei/idea_test/CopyOfApp.java
@@ -0,0 +1,14 @@
+package com.wangwei.idea_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class CopyOfApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
