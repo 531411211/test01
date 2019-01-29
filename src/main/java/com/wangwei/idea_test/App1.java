55ff9417c14947553ceb76e7d1e4d3311d9ac377
“wei wang <wangwei@egridcloud.com>
2019-01-29 14:49:03 +0800

    ***********

---
+++ src/main/java/com/wangwei/idea_test/App1.java
@@ -0,0 +1,13 @@
+package com.wangwei.idea_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App1 
+{
+    public static void main( String[] args )
+    {
+        System.out.println();  
+    }
+}
