7ed8e43d6c30b3ee27aaa053bbad9d9f61c31c28
531411211 <greatgreathaishang@163.com>
2019-04-15 15:57:04 +0800

    Update App1.java

--- src/main/java/com/wangwei/idea_test/App1.java
+++ src/main/java/com/wangwei/idea_test/App1.java
@@ -10,6 +10,7 @@ public class App1
     {
         System.out.println("----------------------");
         System.out.println("----------------------");
-        System.out.println("----------------------");
+        System.out.println("----------------------A");
+        
     }
 }
