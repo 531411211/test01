ca92d00b8b53f123c3bc64b7e076f2c637524bb8
yaoayao <yaoayao@192.168.191.5>
2019-04-15 15:16:14 +0800

    test01

--- src/main/java/com/wangwei/idea_test/App1.java
+++ src/main/java/com/wangwei/idea_test/App1.java
@@ -8,6 +8,8 @@ public class App1
 {
     public static void main( String[] args )
     {
-        System.out.println();  
+        System.out.println("----------------------");
+        System.out.println("----------------------");
+        System.out.println("----------------------");
     }
 }
